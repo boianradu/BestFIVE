c3e87c3c30b5758eb34e6fbc905ce4c951caa472
Radu Boian <boianradu@gmail.com>
2020-02-06 00:00:19 +0200

    feat: resize and other exercises with moviepy

--- src/main.py
+++ src/main.py
@@ -1,10 +1,30 @@
 from video_manager import *
-
+import os
+import glob
+from natsort import natsorted
+from moviepy.editor import *
+from moviepy.video.fx.resize import resize
 
 def main():
-    videoManager = VideoManager()
-    videoManager.format_image("daium")
+    # videoManager = VideoManager()
+    # videoManager.format_image("daium") 
+    gif_name = 'pic'
+    fps = 24
 
+    file_list = glob.glob('D:\\Work\\BestFIVE\\assets\\*.jpg')  # Get all the pngs in the current directory
+    file_list_sorted = natsorted(file_list,reverse=False)  # Sort the images
+    clips = []
+    for i, item in enumerate(file_list_sorted):
+        if i%2==0:
+            clip = ImageClip(item).set_pos((100,0))
+        else:   
+            clip = ImageClip(item).set_pos((0,110))
+            clip = resize(clip, width=400,height=300)
+        clip = clip.set_duration(1)
+        clips.append(clip) 
+
+    concat_clip = concatenate_videoclips(clips, method="compose")
+    concat_clip.write_videofile("test.mp4", fps=fps)
 
 if __name__ == "__main__":
     main()
--- src/video_manager.py
+++ src/video_manager.py
@@ -1,27 +1,22 @@
 from moviepy.editor import *
-import gizeh
-import numpy as np
 from constants import CONSTANTS
+import cv2
+import numpy as np
+import glob
+
 
 class VideoManager:
     """VideoManager handles the process of generating videos. 
     It uses classes audio, media and text."""
 
-    def __init__(self):
-        super(VideoManager, self).__init__()
-        self.TAG = "Video Manager"
-        self.movieClip = VideoClip(make_frame=self.make_frame, duration=3)
-        # self.movieClip.size=(1280,720)
-        self.CONSTANTS=CONSTANTS.getInstance()
-        print("Current class:", self.TAG)
-
+    def __init__(self): 
+        print("VM")
     # def set_background(self):
-    def make_frame(self, t):
-        surface = gizeh.Surface(1280,720) # width, height
-        # radius = W*(1+ (t*(2-t))**2 )/6 # the radius varies over time
-        # circle = gizeh.circle(radius, xy = (64,64), fill=(1,0,0))
-        # circle.draw(surface)
-        return surface.get_npimage() # returns a 8-bit RGB array
-
-    def format_image(self, path):
-        self.movieClip.set_duration(3).write_videofile("movie.mp4", 30) 
+    # def make_frame(self, t): 
+    # def format_image(self, path):
+    #     img_array = []
+    #     for filename in glob.glob('../assets/*.png'):
+    #         img = cv2.imread(filename)
+    #         height, width, layers = img.shape
+    #         size = (width,height)
+    #         img_array.append(img)
